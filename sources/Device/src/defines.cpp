6bd623e5ee05695c394ede795ce265e8ea9cf50d
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-30 14:24:53 +0300

    Пробуем автоинкремент версии

---
+++ sources/Device/src/defines.cpp
@@ -0,0 +1,11 @@
+// 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+
+#ifndef VERSION_BUILD
+
+#define VERSION_BUILD 2
+
+#define DATE_BUILD "2025-10-30 14:24:53"
+
+#define VERSION_SCPI  "1.0"
+
+#endif
