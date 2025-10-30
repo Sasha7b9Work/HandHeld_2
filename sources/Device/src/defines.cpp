3f4755c0925fe33da9dff2198f7142d2c5abd6ff
Sasha7b9Work <Phuntik-it@tut.by>
2025-10-30 14:28:06 +0300

    *

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 2
+#define VERSION_BUILD 3
 
-#define DATE_BUILD "2025-10-30 14:24:53"
+#define DATE_BUILD "2025-10-30 14:28:06"
 
 #define VERSION_SCPI  "1.0"
 
