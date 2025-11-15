673de49cea668f3e3d5229b29eb9bbc41c963f49
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-15 08:37:05 +0300

    Файлы CMSIS обновлены до последней версии с https://github.com/ARM-software/CMSIS_6

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 38
+#define VERSION_BUILD 39
 
-#define DATE_BUILD "2025-11-15 08:27:26"
+#define DATE_BUILD "2025-11-15 08:37:04"
 
 #define VERSION_SCPI  "1.0"
 
