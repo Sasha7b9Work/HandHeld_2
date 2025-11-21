239840e4587b9b14b2188ac224b86e18db2d3aad
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-21 20:30:57 +0300

    v1

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 91
+#define VERSION_BUILD 92
 
-#define DATE_BUILD "2025-11-21 17:45:10"
+#define DATE_BUILD "2025-11-21 20:30:57"
 
 #define VERSION_SCPI  "1.0"
 
