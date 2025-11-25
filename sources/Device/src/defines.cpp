1bff545a71304cc4df87a6f383885c3c27878e77
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-25 21:21:06 +0300

    Удалены неиспользуемые проекты из Transmitter

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 109
+#define VERSION_BUILD 110
 
-#define DATE_BUILD "2025-11-25 21:18:57"
+#define DATE_BUILD "2025-11-25 21:21:06"
 
 #define VERSION_SCPI  "1.0"
 
