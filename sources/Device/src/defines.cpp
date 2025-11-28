3ec0e2d7e50b442c6d04388693f0926fb0bec010
Sasha7b9Work <Phuntik-it@tut.by>
2025-11-28 14:42:26 +0300

    refact

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 153
+#define VERSION_BUILD 154
 
-#define DATE_BUILD "2025-11-28 14:38:16"
+#define DATE_BUILD "2025-11-28 14:42:26"
 
 #define VERSION_SCPI  "1.0"
 
--- sources/Loader/src/Upgrader.cpp
+++ sources/Loader/src/Upgrader.cpp
@@ -1,39 +1,24 @@
 // 2025/11/24 09:42:09 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Upgrader.h"
-#include "Modules/LED/LED.h"
 #include "Hardware/Timer.h"
 #include "Modules/PAN3060/PAN3060.h"
 #include "Display/Display7735.h"
-
-
-namespace Upgrader
-{
-    static void NotifyBeginUpgrade();
-}
 
 
 void Upgrader::Run()
 {
     PAN3060::Init();
 
-    LED::Init();
-
     Display::Init();
 
     TimeMeterMS meter;
 
-    while (meter.ElapsedTime() < 3000)
+    while (meter.ElapsedTime() < 3000 && !PAN3060::InProcessUpgrade())
     {
         Display::Update();
 
         PAN3060::Update();
-
-        if (PAN3060::InProcessUpgrade())
-        {
-            NotifyBeginUpgrade();
-            break;
-        }
     }
 
     if (PAN3060::InProcessUpgrade())
@@ -45,26 +30,4 @@ void Upgrader::Run()
             PAN3060::Update();
         }
     }
-    else
-    {
-
-    }
-}
-
-
-void Upgrader::NotifyBeginUpgrade()
-{
-    LED::Enable(ColorLED::Red);
-
-    Timer::Delay(500);
-
-    LED::Enable(ColorLED::Green);
-
-    Timer::Delay(500);
-
-    LED::Enable(ColorLED::Blue);
-
-    Timer::Delay(500);
-
-    LED::Disable();
 }
