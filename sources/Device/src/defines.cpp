427bea918bcaa0d364d2f374c071f5936cb2ba05
Sasha7b9Work <Phuntik-it@tut.by>
2025-11-28 17:16:27 +0300

    Не помещается прошивка загрузчика в 8 кБ

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 163
+#define VERSION_BUILD 164
 
-#define DATE_BUILD "2025-11-28 17:14:42"
+#define DATE_BUILD "2025-11-28 17:16:27"
 
 #define VERSION_SCPI  "1.0"
 
