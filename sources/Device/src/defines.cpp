80c2e76aab42072f0184cfc62238246f916d72b0
Sasha7b9Work <Sasha7b9@tut.by>
2025-11-28 19:56:47 +0300

    Есть отладочная версия без дисплея (с дисплеем не влазит)

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 176
+#define VERSION_BUILD 177
 
-#define DATE_BUILD "2025-11-28 19:55:37"
+#define DATE_BUILD "2025-11-28 19:56:46"
 
 #define VERSION_SCPI  "1.0"
 
