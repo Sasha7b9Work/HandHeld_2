79e80788300eb41122a1047eac64f01848e2353c
Sasha7b9Work <Phuntik-it@tut.by>
2025-11-28 09:13:57 +0300

    Loader Размер занимаемого ПЗУ ограничен 8 кБ

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 149
+#define VERSION_BUILD 150
 
-#define DATE_BUILD "2025-11-28 09:11:26"
+#define DATE_BUILD "2025-11-28 09:13:57"
 
 #define VERSION_SCPI  "1.0"
 
