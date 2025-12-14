c67a55a25c0fc61a8264ff99599fbbe8e5b26b4f
Sasha7b9 <Sasha7b9@tut.by>
2025-12-15 00:15:42 +0300

    detrash

--- sources/Device/src/Modules/PAN3060/PAN3060.cpp
+++ sources/Device/src/Modules/PAN3060/PAN3060.cpp
@@ -66,27 +66,6 @@ void PAN3060::InitSPI()
 
     pinSPI1_MOSI.ToLow();
     pinSPI1_CLK.ToLow();
-
-//    //                                                                   SCK           MISO          MOSI
-//    gpio_mode_set          (GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE,     GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
-//    gpio_output_options_set(GPIOB, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
-//    gpio_af_set            (GPIOB, GPIO_AF_0,                        GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15);
-//
-//    spi_parameter_struct spi_init_struct;
-//
-//    spi_i2s_deinit(SPI_PAN3060);
-//    spi_struct_para_init(&spi_init_struct);
-//
-//    spi_init_struct.trans_mode = SPI_TRANSMODE_FULLDUPLEX;
-//    spi_init_struct.device_mode = SPI_MASTER;
-//    spi_init_struct.frame_size = SPI_FRAMESIZE_8BIT;
-//    spi_init_struct.clock_polarity_phase = SPI_CK_PL_LOW_PH_1EDGE;
-//    spi_init_struct.nss = SPI_NSS_SOFT;
-//    spi_init_struct.prescale = SPI_PSC_8;
-//    spi_init_struct.endian = SPI_ENDIAN_MSB;
-//    spi_init(SPI_PAN3060, &spi_init_struct);
-//
-//    spi_enable(SPI_PAN3060);
 }
 
 
