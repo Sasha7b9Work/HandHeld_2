5345d6a6c4f05613ff928a34c632221b9fce3048
Sasha7b9 <Sasha7b9@tut.by>
2026-01-26 20:42:36 +0300

    *

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 290
+#define VERSION_BUILD 291
 
-#define DATE_BUILD "2026-01-26 20:36:40"
+#define DATE_BUILD "2026-01-26 20:42:35"
 
 #define VERSION_SCPI  "1.0"
 
