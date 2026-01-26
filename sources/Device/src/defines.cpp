df2669c9e69b6f849949a7882d2a146bd2ebe51e
Sasha7b9Work <Phuntik-it@tut.by>
2026-01-26 14:18:50 +0300

    Для версии дисплея 7735 (мобильная) сделан отдельный проект

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 262
+#define VERSION_BUILD 263
 
-#define DATE_BUILD "2026-01-26 13:53:29"
+#define DATE_BUILD "2026-01-26 14:18:50"
 
 #define VERSION_SCPI  "1.0"
 
