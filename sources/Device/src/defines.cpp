14a111522fa3d155677e7d9ea9e1f70bbd407e61
Sasha7b9 <Sasha7b9@tut.by>
2026-01-26 20:59:32 +0300

    Положен драйвер ST7789 из esp32-st7789-demo-master

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 291
+#define VERSION_BUILD 292
 
-#define DATE_BUILD "2026-01-26 20:42:35"
+#define DATE_BUILD "2026-01-26 20:59:32"
 
 #define VERSION_SCPI  "1.0"
 
