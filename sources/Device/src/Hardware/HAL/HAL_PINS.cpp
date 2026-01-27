742f5c730723abc190ca43d352b1e586d29e1462
Sasha7b9 <Sasha7b9@tut.by>
2026-01-27 20:23:15 +0300

    *

--- sources/Device/src/Hardware/HAL/HAL_PINS.cpp
+++ sources/Device/src/Hardware/HAL/HAL_PINS.cpp
@@ -19,14 +19,26 @@ void Pin::Init()
 
 void PinOut::Init()
 {
+#ifdef MODEL7735
     gpio_mode_set(port, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, pin);
     gpio_output_options_set(port, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, pin);
+#endif
+
+#ifdef MODEL7789
+    gpio_init(port, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, pin);
+#endif
 }
 
 
 void PinIn::Init()
 {
+#ifdef MODEL7735
     gpio_mode_set(port, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, pin);
+#endif
+
+#ifdef MODEL7789
+    gpio_init(port, GPIO_MODE_IPU, GPIO_OSPEED_50MHZ, pin);
+#endif
 }
 
 
