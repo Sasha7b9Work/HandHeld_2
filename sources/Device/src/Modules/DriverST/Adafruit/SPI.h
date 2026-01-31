227029adf9c7b5c4a5a2609bcbba5413b5b5f622
Sasha7b9 <Sasha7b9@tut.by>
2026-01-31 13:27:32 +0300

    *

---
+++ sources/Device/src/Modules/DriverST/Adafruit/SPI.h
@@ -0,0 +1,18 @@
+// 2026/01/31 13:19:28 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+static const uint8_t SPI_MODE0 = 0;
+static const uint8_t LOW = 0;
+static const uint8_t HIGH = 1;
+
+typedef bool boolean;
+
+
+class SPIClass
+{
+
+};
+
+
+void digitalWrite(int8_t, uint8_t);
