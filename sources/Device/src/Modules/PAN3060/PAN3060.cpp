6132309db24d54145fa5bff32142fb048c65105f
Sasha7b9 <Sasha7b9@tut.by>
2026-02-02 22:45:51 +0300

    Возврат к версии, где PAN3060 не переходит в сон

--- sources/Device/src/Modules/PAN3060/PAN3060.cpp
+++ sources/Device/src/Modules/PAN3060/PAN3060.cpp
@@ -1,6 +1,5 @@
 // 2024/03/01 22:46:05 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
-#include "Hardware/HAL/HAL.h"
 #include "Hardware/HAL/HAL_PINS.h"
 #include "Hardware/Timer.h"
 #include "Modules/PAN3060/PAN3060.h"
@@ -19,7 +18,6 @@ namespace PAN3060
     */
 
     static bool need_rx = false;
-    static uint8 irq = 0;           // Сюда читаем значение регистра прямо в прерывании, чтобы потом его обрабатывать
 
     static void InitIRQ();
 
@@ -36,34 +34,23 @@ void PAN3060::InitFull()
 
     InitSPI();
 
-    InitRF();
-}
-
-
-void PAN3060::InitRF()
-{
     rf_init();
 
     rf_set_default_para();
 
     rf_enter_continous_rx();
-
-#ifdef MODEL7735
-    syscfg_exti_line_config(EXTI_SOURCE_GPIOA, EXTI_SOURCE_PIN8);
-    exti_interrupt_flag_clear(EXTI_8);
-#endif
 }
 
 
 void PAN3060::InitIRQ()
 {
 #ifdef MODEL7735
-
     // Инициализируем пин клоков от приёмника на прерывание
     gpio_mode_set(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_PULLUP, GPIO_PIN_8);
     nvic_irq_enable(EXTI4_15_IRQn, 2);
+    syscfg_exti_line_config(EXTI_SOURCE_GPIOA, EXTI_SOURCE_PIN8);
     exti_init(EXTI_8, EXTI_INTERRUPT, EXTI_TRIG_RISING);
-
+    exti_interrupt_flag_clear(EXTI_8);
 #endif
 }
 
@@ -81,16 +68,19 @@ void PAN3060::InitSPI()
 
 void PAN3060::Update()
 {
-    if (need_rx && irq != 0x00)
+    if (need_rx)
     {
         need_rx = false;
 
-        if (irq & REG_IRQ_RX_TIMEOUT)
+        uint8_t _irq;
+
+        _irq = rf_read_spec_page_reg(PAGE0_SEL, 0x6C);
+        if (_irq & REG_IRQ_RX_TIMEOUT)
         {
             rf_clr_irq();
         }
 
-        if (irq & REG_IRQ_RX_DONE)
+        if (_irq & REG_IRQ_RX_DONE)
         {
             uint8_t _buffer[PACKET_PAYLOAD_LENGTH];
             uint8_t _len = rf_read_spec_page_reg(PAGE1_SEL, 0x7D);
@@ -126,36 +116,21 @@ void PAN3060::Update()
                 rf_enter_continous_rx();
             }
         }
-
-        irq = 0x00;
     }
 }
 
 
 void PAN3060::PrepareToSleep()
 {
-#ifdef MODEL7735
-    syscfg_exti_line_clear(EXTI_SOURCE_PIN8);
-#endif
-
-//    rf_deepsleep();
 }
 
 
 void PAN3060::CallbackOnIRQ()
 {
-    irq = rf_read_spec_page_reg(PAGE0_SEL, 0x6C);
-
-    if(irq != 0x00)                 // После захода в спящий режим вызывается это прерывание
-    {                               // Но чтение регистра прерываний даёт 0. Проверяем, что это прерывание вызвано именно приёмом даных
-        need_rx = true;
-
-        InitRF();
-    }
+    need_rx = true;
 }
 
 
 void PAN3060::CallbackOnWakeUp()
 {
-    InitRF();
 }
