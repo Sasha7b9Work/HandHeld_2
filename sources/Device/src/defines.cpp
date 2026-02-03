fb3c796df51d19cad65db643a0d0736aba635385
Sasha7b9 <Sasha7b9@tut.by>
2026-02-03 23:01:36 +0300

    *

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 403
+#define VERSION_BUILD 404
 
-#define DATE_BUILD "2026-02-03 20:43:29"
+#define DATE_BUILD "2026-02-03 23:01:36"
 
 #define VERSION_SCPI  "1.0"
 
