0507a05050bbf8399088b73d31f3d795ec6ed825
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-09 10:35:47 +0300

    Отключены загрузчики в vs_keil

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 441
+#define VERSION_BUILD 442
 
-#define DATE_BUILD "2026-02-08 16:54:13"
+#define DATE_BUILD "2026-02-09 10:35:47"
 
 #define VERSION_SCPI  "1.0"
 
