0e67fd768f67ccb020132170d4fd7748c2d6f077
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-10 13:41:36 +0300

    *

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 491
+#define VERSION_BUILD 492
 
-#define DATE_BUILD "2026-02-10 13:23:19"
+#define DATE_BUILD "2026-02-10 13:41:36"
 
 #define VERSION_SCPI  "1.0"
 
