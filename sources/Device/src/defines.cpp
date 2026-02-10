e41b2727ac95bd398f028465d2f6bd282b5255ba
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-10 09:45:47 +0300

    detrash

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -2,9 +2,9 @@
 
 #ifndef VERSION_BUILD
 
-#define VERSION_BUILD 478
+#define VERSION_BUILD 479
 
-#define DATE_BUILD "2026-02-10 09:42:08"
+#define DATE_BUILD "2026-02-10 09:45:47"
 
 #define VERSION_SCPI  "1.0"
 
--- sources/Device/src/option.h
+++ sources/Device/src/option.h
@@ -1,8 +1,6 @@
 // 2024/03/23 13:22:07 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
 
-#define VERSION 5
-
 #ifdef GUI
     #define ENABLE_EMULATOR
 #endif
