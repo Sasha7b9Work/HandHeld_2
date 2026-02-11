28ccb31d49d7156242d7dd179a36dca32ff0e5b9
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-11 12:22:29 +0300

    Попытка избавиться от мусора на экране при включении

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 515
+#define VERSION_BUILD 516
 
-#define DATE_BUILD "2026-02-11 12:22:06"
+#define DATE_BUILD "2026-02-11 12:22:29"
