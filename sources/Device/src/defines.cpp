033c2e06724162d6d055a5b3a6e11e3ef83e74b0
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-13 08:43:05 +0300

    7789 В каталог проекта положены файлы шрифтов ГОСТ16 и ГОСТ28 из Ч3-96Э

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 517
+#define VERSION_BUILD 518
 
-#define DATE_BUILD "2026-02-13 08:42:15"
+#define DATE_BUILD "2026-02-13 08:43:05"
