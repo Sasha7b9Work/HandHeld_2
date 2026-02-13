3ef380ae651f090f52926b3007f2ee5549409b03
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-13 08:42:15 +0300

    7789 В каталог проекта положены файлы шрифтов FontMid и FontMid из Ч3-96Э

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 516
+#define VERSION_BUILD 517
 
-#define DATE_BUILD "2026-02-11 12:22:29"
+#define DATE_BUILD "2026-02-13 08:42:15"
