b999591038b8b68cec97cb37c128f436cca87e10
Sasha7b9Work <Phuntik-it@tut.by>
2026-02-25 17:01:36 +0300

    Передаётся реальный номер карты

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 574
+#define VERSION_BUILD 575
 
-#define DATE_BUILD "2026-02-25 16:17:52"
+#define DATE_BUILD "2026-02-25 17:01:36"
