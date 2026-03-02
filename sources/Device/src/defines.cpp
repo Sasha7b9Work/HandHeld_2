1536e634513e55cd4ca129da212c9ad648cdda21
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-02 13:26:18 +0300

    win-версия зацикливается на индикаторе

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 585
+#define VERSION_BUILD 586
 
-#define DATE_BUILD "2026-03-02 12:34:05"
+#define DATE_BUILD "2026-03-02 13:26:17"
