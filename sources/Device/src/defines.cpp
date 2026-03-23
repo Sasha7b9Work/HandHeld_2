ad7a71108d2a65786e70a5a9e83556f7c4788ead
Sasha7b9Work <Phuntik-it@tut.by>
2026-03-23 14:32:25 +0300

    *

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 606
+#define VERSION_BUILD 607
 
-#define DATE_BUILD "2026-03-23 14:26:13"
+#define DATE_BUILD "2026-03-23 14:32:25"
