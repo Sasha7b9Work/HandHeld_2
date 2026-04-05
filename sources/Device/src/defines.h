cca51faab578714a51e4b01a691f5df05554ecef
Sasha7b9 <Sasha7b9@tut.by>
2026-04-05 17:43:38 +0300

    7735 Выходит из сна по сигналу

--- sources/Device/src/defines.h
+++ sources/Device/src/defines.h
@@ -7,9 +7,9 @@
 // Отключить контроль напряжения на отладочной плате
 //#define POWER_CONTROL_DISABLE
 
-#define VERSION_BUILD 695
+#define VERSION_BUILD 696
 
-#define DATE_BUILD "2026-04-05 17:42:32"
+#define DATE_BUILD "2026-04-05 17:43:37"
 
 // Врем гашения дисплея после последней кнопки
 #define TIME_BLANK_DISPLAY  20000
