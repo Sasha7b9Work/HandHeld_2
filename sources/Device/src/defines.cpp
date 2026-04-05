f430302a7847da504c13721d150f090c99b65fbe
Sasha7b9 <Sasha7b9@tut.by>
2026-04-05 10:49:07 +0300

    *

--- sources/Device/src/defines.cpp
+++ sources/Device/src/defines.cpp
@@ -1,5 +1,5 @@
 // 2024/11/11 10:49:47 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 
-#define VERSION_BUILD 628
+#define VERSION_BUILD 679
 
-#define DATE_BUILD "2026-04-05 10:45:40"
+#define DATE_BUILD "2026-04-05 10:49:07"
