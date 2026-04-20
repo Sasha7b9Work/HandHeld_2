fb4970c94159e31b6f58f72c543a7ea026b15cc2
Sasha7b9 <Sasha7b9@tut.by>
2026-04-20 07:46:09 +0300

    vs_win линкуется

--- sources/Device/src/defines.h
+++ sources/Device/src/defines.h
@@ -7,9 +7,9 @@
 // Отключить контроль напряжения на отладочной плате
 //#define POWER_CONTROL_DISABLE
 
-#define VERSION_BUILD 704
+#define VERSION_BUILD 705
 
-#define DATE_BUILD "2026-04-20 07:43:45"
+#define DATE_BUILD "2026-04-20 07:46:08"
 
 // Врем гашения дисплея после последней кнопки
 #define TIME_BLANK_DISPLAY  20000
